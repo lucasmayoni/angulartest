f46be4f41352717aca5e11399f086c9525524dc6
Lucas Mayoni <lucasmayoni@gmail.com>
2016-03-28 20:16:42 -0300

    Fix

--- app/login/login.js
+++ app/login/login.js
@@ -11,18 +11,22 @@ angular.module('myApp.login', ['ngRoute','door3.css','angularSoap'])
     }])
 
     .factory("testService", ['$soap',function($soap){
-        var base_url= 'http://dev.redmondsupport.com:8080/SecurityWebServices/SecurityService.wsdl';
+        var base_url= 'http://dev.redmondsupport.com:8080/SecurityWebServices/SecurityService';
         return {
             login:function (userName,password){
                 return $soap.post(base_url, "login", {userName: userName,password:password});
             }
         }
     }])
-
-    .controller('LoginCtrl', [function($scope, testService) {
+    .controller('LoginCtrl', ['$http',function($http){
+        this.postForm = function(){
+            
+        }
+    }]
+    .controller('LoginCtrl',function($scope, testService) {
         testService.login(
             $scope.userName, $scope.password).then(function(response){
             $scope.response = response;
         }
         )
-    }]);
+    });
