d103e8a1ce9985383dcd527adfb22e142516859c
Lucas Mayoni <lucasmayoni@gmail.com>
2016-03-28 07:20:03 -0300

    TEST

---
+++ app/login/login.js
@@ -0,0 +1,28 @@
+'use strict';
+
+angular.module('myApp.login', ['ngRoute','door3.css','angularSoap'])
+
+    .config(['$routeProvider', function($routeProvider) {
+        $routeProvider.when('/login', {
+            templateUrl: 'login/login.html',
+            controller: 'LoginCtrl',
+            css: 'login/login.css'
+        });
+    }])
+
+    .factory("testService", ['$soap',function($soap){
+        var base_url= 'http://dev.redmondsupport.com:8080/SecurityWebServices/SecurityService.wsdl';
+        return {
+            login:function (userName,password){
+                return $soap.post(base_url, "login", {userName: userName,password:password});
+            }
+        }
+    }])
+
+    .controller('LoginCtrl', [function($scope, testService) {
+        testService.login(
+            $scope.userName, $scope.password).then(function(response){
+            $scope.response = response;
+        }
+        )
+    }]);
